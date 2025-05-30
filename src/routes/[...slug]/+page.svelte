2a60cc8137677c826f31d5406ec6ef6a5e5a28aa
tacomilkshake <1824902+tacomilkshake@users.noreply.github.com>
2025-05-30 11:21:44 -0700

    Add docs preview site capability (#6)
    * Add docs preview site capability
    
    * Adjust nav styling

---
+++ src/routes/[...slug]/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+	import { parseMarkdown } from '$lib/markdown.js'
+
+	export let data
+
+	$: ({ html, frontmatter, toc } = parseMarkdown(data.content))
+</script>
+
+<svelte:head>
+	<title>{frontmatter.title || 'Orb Documentation'}</title>
+	{#if frontmatter.metaDescription}
+		<meta name="description" content={frontmatter.metaDescription} />
+	{/if}
+</svelte:head>
+
+<article class="article-content">
+	{#if toc}
+		<div class="toc-container">
+			{@html toc}
+		</div>
+	{/if}
+	{@html html}
+</article>
